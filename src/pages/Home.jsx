f33174c7be94d26135f1e1977c504d3a03dec1c5
mussameeks <mussameeks@gmail.com>
2025-06-06 01:24:44 +0200

    Add files via upload

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,16 +6,17 @@ import MatchList from "../components/MatchList";
 import Footer from "../components/Footer";
 
 export default function Home() {
-  // Just a single state for all/favorites/live/finished/upcoming
   const [activeTab, setActiveTab] = useState("all");
 
   return (
-    <>
+    <div className="min-h-screen flex flex-col">
       <Header />
       <SportTabs />
       <MainFilters activeTab={activeTab} setActiveTab={setActiveTab} />
       <MatchList activeTab={activeTab} />
-      <Footer />
-    </>
+      <div className="mt-auto">
+        <Footer />
+      </div>
+    </div>
   );
 }
